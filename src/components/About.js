b422d1becdfef086e2d6f93e515601dad29960eb
Terence Holmes <piece4ulmind@gmail.com>
2022-06-04 20:47:23 -0400

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {image} from "../data/user.js";
+
 
 function About() {
   return (
@@ -9,3 +11,4 @@ function About() {
     </div>
   );
 }
+export default About;
